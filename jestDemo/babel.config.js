4a97ff1d10744bc5c834e6eb9a7e4b7223dd50fb
feynman <2901954062@qq.com>
2022-05-26 19:16:00 +0800

    ci: babel config add

---
+++ jestDemo/babel.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: fg
+ * @Date: 2022-05-26 19:08:36
+ * @LastEditors: fg
+ * @LastEditTime: 2022-05-26 19:08:36
+ * @Description: babel.config.js
+ */
+module.exports = {
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]]
+}
