2579f1b1671ab16a4d6aaa6e5d73f3f1ec3dd978
feynman <2901954062@qq.com>
2022-07-08 19:06:18 +0800

    feat: expect 期望api upload

---
+++ jestDemo/test/experience/expect.test.js
@@ -0,0 +1,78 @@
+
+/*
+ * @Author: fg
+ * @Date: 2022-07-08 17:21:27
+ * @LastEditors: fg
+ * @LastEditTime: 2022-07-08 18:11:23
+ * @Description: expect
+ */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+test('the best flavor is grapefruit', () => {
+  expect('grapefruit').toBe('grapefruit')
+})
+
+// expect.extend(matchers)可以将自己的matcher添加到jest对象中
+
+expect.extend({
+  toBeWithinRange (received, floor, ceiling) {
+    const pass = received >= floor && received <= ceiling
+    if (pass) {
+      return {
+        message: () => `expected ${received} not to be within range ${floor}-${ceiling}`,
+        pass: true
+      }
+    } else {
+      return {
+        message: `expected ${received} to be within range ${floor} - ${ceiling}`,
+        pass: false
+      }
+    }
+  }
+})
+
+test('numeric ranges', () => {
+  expect(100).toBeWithinRange(90, 110)
+  expect(101).not.toBeWithinRange(0, 100)
+  expect({ apples: 6, bananas: 3 }).toEqual({
+    apples: expect.toBeWithinRange(1, 10),
+    bananas: expect.not.toBeWithinRange(11, 20)
+  })
+})
+
+// expect.extension 异步 Matcher
+
+function getExternalValueFromRemoteSource () {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(10)
+    })
+  })
+}
+
+expect.extend({
+  async toBeDivisibleByExternalValue (received) {
+    const externalValue = await getExternalValueFromRemoteSource()
+    console.log('externalValue:', externalValue)
+    const pass = received % externalValue === 0
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} not to be divisible by ${externalValue}`,
+        pass: true
+      }
+    } else {
+      return {
+        message: () =>
+          `expected ${received} to be divisible by ${externalValue}`,
+        pass: false
+      }
+    }
+  }
+})
+
+test('is divisible by external value', async () => {
+  await expect(100).toBeDivisibleByExternalValue()
+  await expect(101).not.toBeDivisibleByExternalValue()
+})
