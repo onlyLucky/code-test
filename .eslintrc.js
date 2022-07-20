1d3357b4906e7ef4e62bec289bb6bd072a5346d1
Feynman <2901954062@qq.com>
2022-07-20 15:35:52 +0800

    chore: eslint 配置修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
  * @Author: fg
  * @Date: 2022-04-14 10:21:44
  * @LastEditors: fg
- * @LastEditTime: 2022-05-05 16:24:30
+ * @LastEditTime: 2022-07-20 14:52:20
  * @Description: content
  */
 module.exports = {
