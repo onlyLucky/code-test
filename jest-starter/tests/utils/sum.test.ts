3495bfc8a0a4892e657f60554d0ea018fabbb262
Feynman <2901954062@qq.com>
2022-07-22 17:14:08 +0800

    feat: ts jest add

---
+++ jest-starter/tests/utils/sum.test.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: fg
+ * @Date: 2022-07-22 16:43:56
+ * @LastEditors: fg
+ * @LastEditTime: 2022-07-22 16:54:14
+ * @Description: sum test ts
+ */
+import sum from 'utils/sum'
+
+describe('sum test ts', () => {
+  it('add', () => {
+    expect(sum(1, 1)).toEqual(2)
+  })
+})
