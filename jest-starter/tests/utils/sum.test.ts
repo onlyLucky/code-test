84c0d497aa9900befd351d2c01b7559842f5d429
Feynman <2901954062@qq.com>
2022-07-22 18:28:55 +0800

    style: 覆盖测试upload

--- jest-starter/tests/utils/sum.test.ts
+++ jest-starter/tests/utils/sum.test.ts
@@ -2,10 +2,10 @@
  * @Author: fg
  * @Date: 2022-07-22 16:43:56
  * @LastEditors: fg
- * @LastEditTime: 2022-07-22 16:54:14
+ * @LastEditTime: 2022-07-22 17:58:59
  * @Description: sum test ts
  */
-import sum from 'utils/sum'
+import sum from '@/utils/sum'
 
 describe('sum test ts', () => {
   it('add', () => {
