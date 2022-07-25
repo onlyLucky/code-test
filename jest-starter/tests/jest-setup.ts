42738f0799d985265d182645519c009f382de30c
Feynman <2901954062@qq.com>
2022-07-25 17:42:40 +0800

    perf: 修改logger 打印

--- jest-starter/tests/jest-setup.ts
+++ jest-starter/tests/jest-setup.ts
@@ -2,11 +2,12 @@
  * @Author: fg
  * @Date: 2022-07-23 11:21:48
  * @LastEditors: fg
- * @LastEditTime: 2022-07-25 17:35:48
+ * @LastEditTime: 2022-07-25 17:38:51
  * @Description: 全局 Mock
  */
 // 使用 Jest 的 Spy 和扩展 expect 来 Mock `window.location`
 import "jest-location-mock";
+import mockConsole from "jest-mock-console";
 
 Object.defineProperty(global, 'localStorage', {
   value: {
@@ -32,3 +33,4 @@ jest.spyOn(console, 'log').mockReturnValue();
 jest.spyOn(console, 'info').mockReturnValue();
 jest.spyOn(console, 'warn').mockReturnValue();
 jest.spyOn(console, 'error').mockReturnValue();
+mockConsole()
