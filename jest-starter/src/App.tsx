b66d77efa9b1127465eaff886f8b789156ec068c
Feynman <2901954062@qq.com>
2022-07-26 15:42:10 +0800

    workflow: webpack config 配置，ts配置修改

--- jest-starter/src/App.tsx
+++ jest-starter/src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: fg
  * @Date: 2022-07-26 14:07:40
  * @LastEditors: fg
- * @LastEditTime: 2022-07-26 14:11:05
+ * @LastEditTime: 2022-07-26 15:34:14
  * @Description:  App根组件
  */
 import React from 'react';
@@ -12,7 +12,7 @@ const App = ()=>{
   return (
     <div>
       <h1>Hello</h1>
-      <button>click me</button>
+      <Button>click me</Button>
     </div>
   )
 }
