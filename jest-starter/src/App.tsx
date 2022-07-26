4665913e57c3cc8522b3c33e7b41968af076f854
Feynman <2901954062@qq.com>
2022-07-26 15:32:52 +0800

    feat: react demo add

---
+++ jest-starter/src/App.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: fg
+ * @Date: 2022-07-26 14:07:40
+ * @LastEditors: fg
+ * @LastEditTime: 2022-07-26 14:11:05
+ * @Description:  App根组件
+ */
+import React from 'react';
+import { Button } from 'antd';
+
+const App = ()=>{
+  return (
+    <div>
+      <h1>Hello</h1>
+      <button>click me</button>
+    </div>
+  )
+}
+
+export default App
---
+++ jest-starter/src/index.tsx
@@ -0,0 +1,14 @@
+/*
+ * @Author: fg
+ * @Date: 2022-07-26 14:06:04
+ * @LastEditors: fg
+ * @LastEditTime: 2022-07-26 15:24:41
+ * @Description: React App入口
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App';
+import "antd/dist/antd.css";
+
+
+ReactDOM.render(<App/>, document.getElementById('root'))
