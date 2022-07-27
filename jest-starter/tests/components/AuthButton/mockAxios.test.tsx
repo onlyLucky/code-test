ace8de737cc550d59b2e28b968a61364c33d7f76
Feynman <2901954062@qq.com>
2022-07-27 19:43:52 +0800

    test: TODO axios mock add

--- jest-starter/tests/components/AuthButton/mockAxios.test.tsx
+++ jest-starter/tests/components/AuthButton/mockAxios.test.tsx
@@ -2,6 +2,17 @@
  * @Author: fg
  * @Date: 2022-07-27 14:48:51
  * @LastEditors: fg
- * @LastEditTime: 2022-07-27 14:48:51
+ * @LastEditTime: 2022-07-27 15:00:59
  * @Description: Mock Axios test
  */
+import React from 'react'
+import axios from 'axios'
+import {render, screen} from '@testing-library/react'
+import AuthButton from 'components/AuthButton/index'
+
+// 更偏向细节，效果并不好
+describe('AuthButton Mock Axios', () => {
+  it('可以正确展示普通用户按钮内容',async ()=>{
+    expect(2+2).toEqual(4)
+  })
+})
