0df447ca0eaa20f78621db2a418cf06707e3741e
Feynman <2901954062@qq.com>
2022-07-27 14:50:33 +0800

    test: AuthButton 组件测试提交

--- jest-starter/tests/components/AuthButton/simple.test.tsx
+++ jest-starter/tests/components/AuthButton/simple.test.tsx
@@ -2,6 +2,18 @@
  * @Author: fg
  * @Date: 2022-07-27 13:52:20
  * @LastEditors: fg
- * @LastEditTime: 2022-07-27 13:52:20
+ * @LastEditTime: 2022-07-27 14:40:20
  * @Description: AuthButton simple.test
  */
+import {render, screen} from '@testing-library/react'
+import AuthButton from "components/AuthButton/index"
+import React from 'react'
+
+describe('AuthButton', () => {
+  it('可以正常展示',()=>{
+    render(<AuthButton>登录</AuthButton>)
+    // 这里的toBeDefined不是很靠谱
+    // expect(screen.queryByText('登录')).toBeDefined()
+    expect(screen.getByText('登录')).toBeInTheDocument();
+  })
+})
