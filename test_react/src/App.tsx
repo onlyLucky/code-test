e0c95f9a39b768c05cdeb4e3c1c67c273891f927
Feynman <2901954062@qq.com>
2023-08-16 17:25:34 +0800

    feat: 新增todo demo

---
+++ test_react/src/App.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./App.css";
+import { Todo } from "./components/Todo";
+import { TodoForm } from "./components/TodoForm";
+import { TodoType } from "./types";
+import { toggleOneTodo } from "./utils/toggleOneTodo";
+
+function App() {
+  const [todos, setTodos] = useState<TodoType[]>([
+    {
+      text: "Learn about React",
+      isCompleted: false,
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false,
+    },
+    {
+      text: "Build really cool todo app",
+      isCompleted: false,
+    },
+  ]);
+
+  const addTodo = (text: string) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const toggleTodo = (index: number) => {
+    const newTodos = toggleOneTodo(todos, index);
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index: number) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            toggleTodo={toggleTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
